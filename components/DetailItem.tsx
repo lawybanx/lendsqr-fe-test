5a390718e51fbb1f321d25ad3e77ce35c30812ae
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2024-01-16 19:28:49 +0100

    fix: deployment issues

--- components/DetailItem.tsx
+++ components/DetailItem.tsx
@@ -1,16 +1,11 @@
 interface DetailItemProps {
-  details: [
-    {
-      title: string;
-      value: string;
-    }
-  ];
+  [key: string]: any;
 }
 
 export default function DetailItem({ details }: DetailItemProps) {
   return (
     <div className='detail-item'>
-      {details.map(({ title, value }) => (
+      {details.map(({ title, value }: { [key: string]: any }) => (
         <div key={title} className='item'>
           <p>{title}</p>
           <p>{value}</p>
